f5f84ba273e1f4d217952092705643d6df37a097
LeoCapontes <leo8capontes@hotmail.co.uk>
2025-02-24 18:39:51 +0000

    Add simple rate limiting

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
 const express = require('express')
 const cors = require('cors')
+const rateLimit = require('express-rate-limit')
 require('dotenv').config()
 const PORT = process.env.PORT || 4000
 
 const app = express()
+
+// Rate limiting
+ const limiter = rateLimit({
+  windowMs: 10 * 60 * 1000, // 10 minutes
+  max: 5
+})
+
+app.use(limiter)
+app.set('trust proxy', 1)
 
 //Routes
 app.use('/api', require('./routes'))
